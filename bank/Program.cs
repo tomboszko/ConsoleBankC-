e3e32b15875b462d7cc10b72c030977eb13736f1
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-13 10:47:57 +0100

    Update Program.cs to print "this is my bank" instead of "Hello, World!"

--- bank/Program.cs
+++ bank/Program.cs
@@ -1,4 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
 
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is my bank");
+        }
+    }
+
+}
 
