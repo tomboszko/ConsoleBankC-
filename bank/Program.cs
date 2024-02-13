91917dc608bacf878b151005c126e0d25c837b73
Thomas Gomree <132305348+tomboszko@users.noreply.github.com>
2024-02-13 12:01:23 +0100

    Update binary files and assembly information

--- bank/Program.cs
+++ bank/Program.cs
@@ -1,15 +1,156 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
+﻿using System;
 
-namespace bank
+public class Client
 {
-    class Program
+    public int Id { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public int PinCode { get; set; }
+    public DateTime DateJoined { get; set; }
+
+    public Client(int id, string lastName, string firstName, DateTime dateOfBirth, int pinCode, DateTime dateJoined)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("this is my bank");
-        }
+        Id = id;
+        LastName = lastName;
+        FirstName = firstName;
+        DateOfBirth = dateOfBirth;
+        PinCode = pinCode;
+        DateJoined = dateJoined;
     }
+
+    public void UpdateInfo(string lastName, string firstName, DateTime dateOfBirth, int pinCode)
+{
+    LastName = lastName;
+    FirstName = firstName;
+    DateOfBirth = dateOfBirth;
+    PinCode = pinCode; // 
+}
 
 }
 
+public class BankAccount
+{
+    public Client Client { get; set; }
+    public decimal Balance { get; private set; }
+    public string Type { get; set; }
+
+    public BankAccount(Client client, string type)
+    {
+        Client = client;
+        Type = type;
+    }
+
+    private bool VerifyPin(int inputPin)
+    {
+        return Client.PinCode == inputPin;
+    }
+
+    public decimal CheckBalance(int inputPin)
+    {
+        if (VerifyPin(inputPin))
+        {
+            return Balance;
+        }
+        else
+        {
+            throw new Exception("Invalid PIN.");
+        }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public bool Withdraw(decimal amount, int inputPin)
+    {
+        if (!VerifyPin(inputPin))
+        {
+            throw new Exception("Invalid PIN.");
+        }
+
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+{
+    Client client = new Client(1, "Doe", "John", new DateTime(1980, 1, 1), 1234, DateTime.Now);
+    BankAccount bankAccount = new BankAccount(client, "Savings");
+
+    Console.WriteLine("Please enter your PIN to access your account:");
+    int inputPin = Convert.ToInt32(Console.ReadLine());
+
+    try
+    {
+        if (bankAccount.CheckBalance(inputPin) >= 0) // Juste pour vérifier le PIN
+        {
+            Console.WriteLine("PIN verified.");
+        }
+
+        string action;
+        do
+        {
+            Console.WriteLine("What would you like to do? (deposit, withdraw, info, quit):");
+            action = Console.ReadLine().ToLower();
+
+            switch (action)
+            {
+                case "deposit":
+                    Console.WriteLine("Enter amount to deposit:");
+                    decimal depositAmount = Convert.ToDecimal(Console.ReadLine());
+                    bankAccount.Deposit(depositAmount);
+                    Console.WriteLine($"New balance: {bankAccount.CheckBalance(inputPin)}");
+                    break;
+                case "withdraw":
+                    Console.WriteLine("Enter amount to withdraw:");
+                    decimal withdrawAmount = Convert.ToDecimal(Console.ReadLine());
+                    if (bankAccount.Withdraw(withdrawAmount, inputPin))
+                    {
+                        Console.WriteLine($"New balance: {bankAccount.CheckBalance(inputPin)}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient funds.");
+                    }
+                    break;
+                case "info":
+                    Console.WriteLine("Enter your last name:");
+                    string lastName = Console.ReadLine();
+                    Console.WriteLine("Enter your first name:");
+                    string firstName = Console.ReadLine();
+                    Console.WriteLine("Enter your date of birth (YYYY-MM-DD):");
+                    DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter a new PIN (or old one to keep it):");
+                    int newPin = Convert.ToInt32(Console.ReadLine());
+
+                    client.UpdateInfo(lastName, firstName, dateOfBirth, newPin);
+                    Console.WriteLine("Information updated successfully.");
+                    break;
+                case "quit":
+                    Console.WriteLine("Exiting.");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Please choose deposit, withdraw, info, or quit.");
+                    break;
+            }
+        }
+        while (action != "quit");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occurred: " + ex.Message);
+    }
+}
+}
--- bank/obj/Debug/net8.0/bank.AssemblyInfo.cs
+++ bank/obj/Debug/net8.0/bank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1c4c3d530b3120dc2b135ecb09b9a0cffaac62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e32b15875b462d7cc10b72c030977eb13736f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("bank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
